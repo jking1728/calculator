ce2eb4e74884836da19cd53e4f04d612a7f16ded
Jordan King <supapeachx@gmail.com>
2019-06-04 12:57:33 -0700

    added user interface

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+};
+var substract = function(number1, number2) {
+    return number1 - number2;
+};
+var multiply = function(number1, number2) {
+    return number1 * number2;
+};
+var divide = function(number1, number2) {
+    return number1 / number2;
+};
+
+// Everything below this line is user interface logic:
+
+$(document).ready(function() {
+    $("form#add").submit(function(event) {
+    event.preventDefault();
+        var number1 = parseInt($("#add1").val());
+        var number2 = parseInt($("#add2").val());
+        var result = add(number1, number2);
+        $("#output").text(result);
+    });
+});
